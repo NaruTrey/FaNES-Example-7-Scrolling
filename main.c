5bce77dcbc71c67ac109f94424e2f26ea08d4ec1
NaruTrey <me@ntrey.org>
2015-12-24 16:10:15 +0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <FaNES.h>
+#include "imageLeft.h"
+#include "imageRight.h"
+
+const uchar palette[16] = {
+    0x11, 0x00, 0x10, 0x30,
+    0x01, 0x21, 0x31, 0x30,
+    0x06, 0x16, 0x26, 0x36,
+    0x0A, 0x09, 0x19, 0x29
+};
+
+static uint topScrollX, topScrollY;
+static int topSpeedHorizontal, topSpeedVertical;
+
+int main(void) {
+    setPaletteBackground(palette);
+    unpackNametableRLE(imageLeft, NAMETABLE_A);
+    unpackNametableRLE(imageRight, NAMETABLE_D);
+    onGraphics();
+    while(TRUE) {
+        uchar pad = padPush(PAD_PORT_PLAYER_1);
+        if(pad & PAD_BUTTON_LEFT) --topSpeedHorizontal;
+        if(pad & PAD_BUTTON_RIGHT) ++topSpeedHorizontal;
+        if(pad & PAD_BUTTON_UP) --topSpeedVertical;
+        if(pad & PAD_BUTTON_DOWN) ++topSpeedVertical;
+        topScrollX += topSpeedHorizontal;
+        topScrollY += topSpeedVertical;
+        setBackgroundScrollI(topScrollX, topScrollY);
+        waitFrame();
+    }
+}
